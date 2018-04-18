e9dc79e70503f9766490282346e56eb1f28f16e7
Vyachx <slavik-2709@yandex.ru>
2018-04-18 17:48:48 +0300

    Начал прописывать логику

---
+++ Lesson01/js/logic.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    
+    function randomInteger(min, max) {
+    var rand = min - 0.5 + Math.random() * (max - min + 1)
+    rand = Math.round(rand);
+    return rand;
+  }
+    
+    var valueone = 
+
+
+
+
+});
