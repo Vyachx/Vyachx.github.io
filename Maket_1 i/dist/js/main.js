77da23edb0b69705ab52a0796f48faf2bf3867c0
Vyachx <slavik-2709@yandex.ru>
2018-04-27 15:12:24 +0300

    `

---
+++ Maket_1 i/dist/js/main.js
@@ -0,0 +1,2 @@
+console.log("1js"),console.log("componentss"),console.log("main");
+//# sourceMappingURL=main.js.map
---
+++ Maket_1 i/gulp/fonts.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var config = require('../package.json').config;
+
+gulp.task('fonts', function () {
+
+    return gulp.src(config.src.fonts)
+        .pipe(gulp.dest(config.dist.fonts));
+});
+
+gulp.task('fonts:watch', function () {
+    gulp.watch(config.dist.fonts, ['fonts']);
+});
