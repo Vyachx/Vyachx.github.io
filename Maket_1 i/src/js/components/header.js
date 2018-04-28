f9b63bf087b0cfadbd9916ab9dd8f9aea50dfd10
Vyachx <slavik-2709@yandex.ru>
2018-04-28 23:20:37 +0300

    `

--- Maket_1 i/src/js/components/header.js
+++
@@ -1,9 +0,0 @@
-var btn = $('.header-body__burger'),
-    menu = $('.header-body'),
-    mask = $('.mask');
-function menuShow () {
-    btn.on('click', function(){
-        menu.toggleClass('is-active');
-        mask.toggleClass('is-active');
-    })
-}
--- Maket_1 i/src/js/libs.js
+++
@@ -1,3 +0,0 @@
-//=require ../../node_modules/jquery/dist/jquery.min.js
-//=require libs/slider.js
-//=require libs/filter.js
