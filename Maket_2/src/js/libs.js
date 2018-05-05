53851ca95655d355ee72c60fadd73c10d245cfb6
Vyachx <slavik-2709@yandex.ru>
2018-05-05 11:17:49 +0300

    Готов intro

---
+++ Maket_2/src/js/libs.js
@@ -0,0 +1,4 @@
+//=require ../../node_modules/jquery/dist/jquery.min.js
+//=require libs/slider.js
+//=require libs/filter.js
+
---
+++ Maket_2/src/js/main.js
@@ -0,0 +1,26 @@
+//=require components/**.*
+
+$(window).ready(function () {
+	menuShow();
+
+	$('.intro-list').flickity({
+	  cellAlign: 'left',
+	  contain: true,
+	  prevNextButtons: false
+	});
+
+	$('.features-items').flickity({
+	  cellAlign: 'left',
+	  contain: true,
+	  prevNextButtons: false
+	});
+
+	$('.team-people').flickity({
+	  cellAlign: 'left',
+	  contain: true,
+	  prevNextButtons: false
+	});
+
+	filter();
+
+})
